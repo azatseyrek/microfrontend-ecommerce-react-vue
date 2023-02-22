4ce4c9cf255eb618317bbe1b40db0080efd3ba03
Azat Seyrek <azat.seyrek@adesso.com.tr>
2023-02-22 14:32:51 +0300

    marketing deployment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -16,7 +16,7 @@ const prodConfig = {
       name: 'container',
       //   filename: 'remoteEntry.js', // not needed for container
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
